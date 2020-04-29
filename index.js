863d833591f94fe007550f34c0f01bba45674247
Rajesh Thangavelu <rajesh.t.2@alight.com>
2020-04-29 22:48:40 +0530

    'df'

---
+++ index.js
@@ -0,0 +1,14 @@
+function imgshadow(option) {
+  let images = document.querySelectorAll("imgshadow");
+  if (option.shadow_type == "hard") option.shadow_type = "0px";
+  else option.shadow_type = "15px";
+
+  images.forEach((image) => {
+    image.style.boxShadow =
+      "10px 10px $(option.shadow_type) 1px rgba(0,0,0,0,12)";
+    if (option.padding) {
+      image.style.padding = "1em";
+    }
+  });
+}
+module.exports.imgshadow = imgshadow;
